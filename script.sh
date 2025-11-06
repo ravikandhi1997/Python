d49c6faefbede0dfa53edca406853188ec14cae7
ravikandhi1997 <ravikandhi1997@gmail.com>
2025-11-06 11:25:15 +0530

    files added successfully

---
+++ script.sh
@@ -0,0 +1,6 @@
+git init
+git add aws-s3bucket.py
+git commit -m "Add S3 bucket creation script"
+git branch -M main
+git remote add origin https://github.com/ravikandhi1997/Python.git
+git push -u origin main
